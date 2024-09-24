819baf704e212bf983f635c50eed972fd9a49fa6
ROHIN PRAMOD <rohinpramod320@gmail.com>
2024-09-24 23:00:47 +0530

    commit db

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-const uri = process.env.MongoDB_URI
-
+const uri = 'mongodb+srv://rohinpramod320:2qAMzZxUowWc0Ksl@cluster0.jjho3.mongodb.net/todoList?retryWrites=true&w=majority'
 const connectDB = async ()=>{
     await mongoose.connect(uri)
 }
